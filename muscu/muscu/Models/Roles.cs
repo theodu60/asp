19041d15e3278c957b41c7d1a1835aa91a3e9ed5
Théophile HEMACHANDRA <theodu60@live.Fr>
2017-01-04 10:47:28 +0100

    Roles API OK
    api ok avec un seed auto a chaque build

---
+++ muscu/muscu/Models/Roles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Muscu.Models
+{
+    public class Roles
+    {
+        public int id { get; set; }
+        public string nom { get; set; }
+
+    }
+}
