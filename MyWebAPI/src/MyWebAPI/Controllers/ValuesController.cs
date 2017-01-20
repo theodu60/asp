f644d226e46175968675dcc507ec9fdcebbe73b8
Lenormand <lenormandbapt@gmail.com>
2017-01-21 00:39:52 +0100

    begin

---
+++ MyWebAPI/src/MyWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyWebAPI.Models;
+
+namespace MyWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        public readonly MyDbContext _context;
+
+        public ValuesController(MyDbContext context)
+        {
+            _context = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id == id);
+            if (user != null)
+                return Ok(user) ;
+            return NotFound();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Users value)
+        {
+            _context.Users.Add(value);
+            _context.SaveChanges();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MyWebAPI/src/MyWebAPI/Models/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyWebAPI.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public  DbSet<Users> Users { get; set; }
+    }
+}
