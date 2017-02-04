3c3156c896c61d3f66c34053faf31c894a16122e
Lenormand <lenormandbapt@gmail.com>
2017-02-04 16:19:03 +0100

    Add new Controller
    new users controller

---
+++ MyWebAPI/src/MyWebApp/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApp.Models;
+
+namespace MyWebApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UsersService _service;
+
+        public UsersController(UsersService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Settings()
+        {
+            return View();
+        }
+
+        public IActionResult Users(int id)
+        {
+            return View(_service.Get(id));
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
