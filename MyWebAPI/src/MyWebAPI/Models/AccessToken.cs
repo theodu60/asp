4245b3cbe894964d20587d6b158525ce0db98b5d
Théophile HEMACHANDRA <theodu60@live.Fr>
2017-02-07 21:37:45 +0100

    accesstoken

---
+++ MyWebAPI/src/MyWebAPI/Models/AccessToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebAPI.Models
+{
+    public class AccessToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+
+        public virtual Users Users { get; set; }
+
+    }
+}
--- MyWebAPI/src/MyWebAPI/Models/Users.cs
+++ MyWebAPI/src/MyWebAPI/Models/Users.cs
@@ -8,6 +8,10 @@ namespace MyWebAPI.Models
     public class Users
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public DateTime age { get; set; }
+
+        public virtual AccessToken AccessToken { get; set; }
      }
 }
