59e2ae4ee02ed926d6c0a7759778d68926fc6d38
Lenormand <lenormandbapt@gmail.com>
2017-02-09 16:09:24 +0100

    try to link story to bdd

--- MyWebAPI/src/MyWebAPI/Controllers/StoryController.cs
+++ MyWebAPI/src/MyWebAPI/Controllers/StoryController.cs
@@ -5,42 +5,46 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MyWebAPI.Models;
 
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace MyWebAPI.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class StoryController : Controller
     {
+
         public readonly MyDbContext _context;
 
-        public ValuesController(MyDbContext context)
+        public StoryController(MyDbContext context)
         {
             _context = context;
         }
-        // GET api/values
+
+        // GET: api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value11", "value21" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public IActionResult Get(int id)
+        public string Get(int id)
         {
-            var user = _context.Users.FirstOrDefault(u => u.Id == id);
-            if (user != null)
-                return Ok(user);
+            var story = _context.Story.FirstOrDefault(u => u.Id == id);
+            if (story != null)
+                return Ok(story);
             return NotFound();
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]Users value)
+        public void Post([FromBody]Story value)
         {
-            _context.Users.Add(value);
+            _context.Story.Add(value);
             _context.SaveChanges();
-        } 
-        
+        }
+
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
